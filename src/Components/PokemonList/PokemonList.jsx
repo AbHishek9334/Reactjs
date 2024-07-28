e9cb23be24626f7af67c078db52ce6e788d64ef1
AbHishek9334 <narayana333434@gmail.com>
2024-07-28 12:35:38 +0530

    added pagination

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -6,13 +6,18 @@ import Pokemon from "../Pokemon/Pokemon";
 function PokemonList(){
     const [pokemonList,setPokemonList]=useState([])
     const [isLoading,setIsLoading]=useState(true);
+    const [pokedexUrl,setPokedexUrl]=useState("https://pokeapi.co/api/v2/pokemon")
+    const [nextUrl,setNextUrl]=useState("")
+    const [prevUrl,setPrevUrl]=useState("")
     async function downloadPokemons(){
-        const response=await axios.get("https://pokeapi.co/api/v2/pokemon")
+        const response=await axios.get( pokedexUrl)
         console.log(response)
         const pokemonResults=response.data.results
         const pokemonResultsPromise=pokemonResults.map((pokemon)=>axios.get(pokemon.url))
         const pokemonData=await axios.all(pokemonResultsPromise)
         console.log(pokemonData)
+        setNextUrl(response.data.next)
+        setPrevUrl(response.data.previous)
         const res=pokemonData.map((pokeData)=>{
             const pokemon=pokeData.data;
             return{
@@ -29,7 +34,7 @@ function PokemonList(){
     useEffect(()=>{
         downloadPokemons()
          
-    },[])
+    },[pokedexUrl])
    
     return (
         <div className="pokemon-list-wrapper">
@@ -41,6 +46,10 @@ function PokemonList(){
                 pokemonList.map((p)=><Pokemon name={p.name} image={p.image} key={p.id}/> )
                 
                 }
+            </div>
+            <div className="controls">
+                 <button disabled={prevUrl==null} onClick={()=>setPokedexUrl(prevUrl)}>Prev</button>
+                 <button disabled={nextUrl==null} onClick={()=>setPokedexUrl(nextUrl)}>Next</button>
             </div>
        
         </div>
